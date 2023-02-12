8784d58801986bf7b375a962b5bf68ece2f6428d
kodemystic <bruno.tarrana@fujitsu.com>
2023-02-12 20:18:11 +0000

    1

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,5 +1,7 @@
+/////////////////// - TIMER - /////////////////////
+
 window.startTimer = function (myDuration, display, callback) {
-    let timer = myDuration;
+    let timer = myDuration - 60;
     let minutes;
     let seconds;
     let interval = setInterval(function () {
@@ -16,26 +18,3 @@ window.startTimer = function (myDuration, display, callback) {
 }
 
 console.log("A C T I V E  -  T I M E R . J S");
-
-
-//////////////////////////////////////////////////////////////////////////////////
-
-// function startTimer(startTime, duration, user) {
-//     console.log("A C T I V E  -  S T A R T   T I M E R");
-  
-//     let currentTime = new Date();
-//     let elapsedTime = Math.round((currentTime - new Date(startTime)) / 1000);
-//     let timeLeft = duration * 60 - elapsedTime;
-  
-//     let timerId = `timer-${user}`;
-//     let timer = document.querySelector(`.${timerId}`);
-    
-//     let intervalId = setInterval(function() {
-//       timeLeft--;
-//       timer.innerHTML = `${Math.floor(timeLeft / 60)}:${(timeLeft % 60).toString().padStart(2, "0")}`;
-      
-//       if (timeLeft <= 0) {
-//         clearInterval(intervalId);
-//       }
-//     }, 1000);
-//   }
