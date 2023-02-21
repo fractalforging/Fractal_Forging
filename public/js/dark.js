3cddbf7b46295631f447d6036d6b5b4217ac0a8b
kodemystic <bruno.tarrana@fujitsu.com>
2023-02-22 00:19:45 +0100

    3

--- public/js/dark.js
+++ public/js/dark.js
@@ -1,46 +1,40 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
 
-const toggleBtnSun = document.getElementById('toggle-mode-btn-sun');
-const toggleBtnMoon = document.getElementById('toggle-mode-btn-moon');
-const body = document.getElementsByTagName('body')[0];
-const isDarkMode = JSON.parse(localStorage.getItem('isDarkMode'));
-const iconSun = toggleBtnSun.querySelector("i");
-const iconMoon = toggleBtnMoon.querySelector("i");
+  // Set initial mode and body
+  const body = document.getElementsByTagName('body')[0];
 
-// initialize the page with the saved state of dark mode
-if (localStorage.getItem('isDarkMode') === 'true') {
-  body.classList.add('dark-mode');
-  // icon.classList.add("fa-sharp", "fa-solid", "fa-moon");
-  // icon.classList.remove("fa-regular", "fa-sun"); 
-  console.log("DARK MODE LOADED");
-} else {
-  body.classList.remove('dark-mode');
-  // icon.classList.remove("fa-sharp", "fa-solid", "fa-moon");
-  // icon.classList.add("fa-regular", "fa-sun"); 
-  console.log("DARK MODE LOADED");
-}
+  // initialize the page with the saved state of dark mode
+  if (localStorage.theme) {
+    if (localStorage.getItem('theme') === 'dark') {
+      body.classList.add('dark-mode');
+      console.log("DARK MODE LOADED FROM START");
+    } else {
+      body.classList.remove('dark-mode');
+      console.log("LIGHT MODE LOADED FROM START");
+    }
+  }
 
-// define the toggle function
-function toggleModeSun() {
+  // define the toggle function
+  function toggleModeSun() {
     body.classList.remove('dark-mode');
-    localStorage.setItem('isDarkMode', false);
-    // icon.classList.remove("fa-sharp", "fa-solid", "fa-moon");
-    // icon.classList.add("fa-regular", "fa-sun"); 
-    console.log("LIGHT MODE LOADED")
+    localStorage.setItem('theme', 'light');
+    console.log("LIGHT MODE LOADED FROM TOGGLE")
   }
 
-function toggleModeMoon() {
+  function toggleModeMoon() {
     body.classList.add('dark-mode');
-    localStorage.setItem('isDarkMode', true);
-    // icon.classList.add("fa-sharp", "fa-solid", "fa-moon");
-    // icon.classList.remove("fa-regular", "fa-sun");  
-    console.log("DARK MODE LOADED")
+    localStorage.setItem('theme', 'dark');
+    console.log("DARK MODE LOADED FROM TOGGLE")
   }
 
-// add event listener to button
-toggleBtnSun.addEventListener('click', toggleModeMoon);
-toggleBtnMoon.addEventListener('click', toggleModeSun);
+  // Get moon and sun buttons
+  const toggleBtnSun = document.getElementById('toggle-mode-btn-sun');
+  const toggleBtnMoon = document.getElementById('toggle-mode-btn-moon');
 
-console.log("A C T I V E  -   D A R K   L O A D E D");
+  // add event listener to button
+  toggleBtnSun.addEventListener('click', toggleModeMoon);
+  toggleBtnMoon.addEventListener('click', toggleModeSun);
+
+  console.log("A C T I V E  -   D A R K   L O A D E D");
 
 });
