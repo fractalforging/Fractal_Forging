72222573d39bd2bb1e7e2339277e4808f4c43366
kodemystic <btarrana@protonmail.ch>
2023-04-27 01:25:02 +0200

    1

--- models/user.js
+++ models/user.js
@@ -3,9 +3,11 @@ const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: String, 
+    password: String,
     roles: { type: String, default: "user" },
     remainingBreakTime: { type: Number, default: 35 * 60 },
+    isOnline: { type: Boolean, default: false },
+    socketId: { type: String, default: null },
 });
 
 userSchema.plugin(passportLocalMongoose);
--- routes/logout.js
+++ routes/logout.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const kleur = require('kleur');
 const logger = require('./logger');
+const { isLoggedIn } = require('../middleware/authentication.js');
 const router = express.Router();
 
-//HANDLING USER LOGOUT
-router.get("/", function (req, res, next) {
+// HANDLING USER LOGOUT
+router.get("/", isLoggedIn, function (req, res, next) {
   const username = req.user.username;
-  req.logout(function (err) {
+  req.logout(async function (err) {
     if (err) {
       logger.error(err);
+    }
+    if (req.user) { // Check if the req.user object is not null
+      req.user.isOnline = false;
+      await req.user.save();
     }
     logger.warn('Logout successful for user: ' + kleur.magenta(username));
     req.session.destroy(function (err) {
