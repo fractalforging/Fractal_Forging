d9b746e5360c3899016c2d3680aa42359c9c74ae
goatchild <40519554+goatchild@users.noreply.github.com>
2023-05-17 19:44:57 +0200

    1

--- routes/changepassword.js
+++ routes/changepassword.js
@@ -15,47 +15,47 @@ changepasswordRoute.post("/", isLoggedIn, async (req, res) => {
     if (!user) {
       req.session.message = "Error";
       logger.error("User not found");
-      return res.render("account", { error: "Error, please try again", currentUser: req.user });
+      return res.render("changepassword", { error: "Error, please try again", currentUser: req.user });
     }
 
     if (!req.body.currentpassword) {
       req.session.message = "Wrong";
       logger.error("Current password empty");
-      return res.render("account", { error: "Current password empty!", currentUser: req.user });
+      return res.render("changepassword", { error: "Current password empty!", currentUser: req.user });
     }
 
     if (req.body.newpassword !== req.body.confirmpassword) {
       req.session.message = "Mismatch";
       logger.error("New password and confirm password do not match");
-      return res.render("account", { error: "New password and confirm password do not match", currentUser: req.user });
+      return res.render("changepassword", { error: "New password and confirm password do not match", currentUser: req.user });
     }
 
     user.authenticate(req.body.currentpassword, (err, valid) => {
       if (err || !valid) {
         req.session.message = "Wrong";
         logger.error("Current password wrong 2");
-        return res.render("account", { error: "Current password incorrect!", currentUser: req.user });
+        return res.render("changepassword", { error: "Current password incorrect!", currentUser: req.user });
       }
 
       user.setPassword(req.body.newpassword, (err) => {
         if (err) {
           req.session.message = "Error";
           logger.error(err);
-          return res.render("account", { error: "Error, please try again", currentUser: req.user });
+          return res.render("changepassword", { error: "Error, please try again", currentUser: req.user });
         }
 
         user.save((err) => {
           if (err) {
             req.session.message = "Error";
             logger.error(err);
-            return res.render("account", { error: "Error, please try again", currentUser: req.user });
+            return res.render("changepassword", { error: "Error, please try again", currentUser: req.user });
           }
 
           req.logIn(user, (err) => {
             if (err) {
               req.session.message = "Error";
               logger.error(err);
-              return res.render("account", { error: "Error, please try again", currentUser: req.user });
+              return res.render("changepassword", { error: "Error, please try again", currentUser: req.user });
             }
             
             req.session.message = "Changed";
@@ -68,13 +68,13 @@ changepasswordRoute.post("/", isLoggedIn, async (req, res) => {
   } catch (err) {
     req.session.message = "Error";
     logger.error(err);
-    return res.render("account", { error: "Error, please try again", currentUser: req.user });
+    return res.render("changepassword", { error: "Error, please try again", currentUser: req.user });
   }
 });
 
 //HANDLING ACCOUNT
 changepasswordRoute.get("/", isLoggedIn, (req, res) => {
-  return res.render("account", { error: 'no error', currentUser: req.user });
+  return res.render("changepassword", { error: 'no error', currentUser: req.user });
 });
 
 export default changepasswordRoute;
