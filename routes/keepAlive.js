5bd56c3413e9a61ec82434a23cc6dac3507572c0
goatchild <40519554+goatchild@users.noreply.github.com>
2023-06-03 20:43:18 +0200

    1

--- routes/keepAlive.js
+++ routes/keepAlive.js
@@ -38,7 +38,7 @@ const sendRequest = (interval) => {
   const req = https.request(options, res => {
     console.log(`[${formatter.format(new Date())}] statusCode: ${res.statusCode}`);
     if (res.statusCode >= 200 && res.statusCode < 300) {
-      console.log(`[${formatter.format(new Date())}] Page touched successfully. Random interval: ${(interval / (60 * 1000)).toFixed(2)} minutes`);
+      console.log(`[${formatter.format(new Date())}] Page touched successfully. Next touch in ${(interval / (60 * 1000)).toFixed(2)} minutes`);
     } else {
       console.error(`[${formatter.format(new Date())}] The server responded with a non-successful status code: ${res.statusCode}`);
     }
