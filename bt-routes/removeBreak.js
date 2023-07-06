4b15c66699f37be8c47d673c6cce7e819705afdf
kodemystic <btarrana@protonmail.ch>
2023-07-06 16:29:19 +0200

    1

--- bt-routes/removeBreak.js
+++ bt-routes/removeBreak.js
@@ -12,19 +12,24 @@ const removeBreak = (io, BreakTrack, User) => {
     const id = req.params.id;
     const beforeStart = req.query.beforeStart === 'true';
     const isAdmin = req.query.isAdmin === 'true';
-    
+
     const session = await mongoose.startSession();
 
     let hasCommitted = false;
 
     try {
       session.startTransaction();
-      
+
       const breakToRemove = await BreakTrack.findById(id).session(session);
       const userToUpdate = await User.findOne({ username: breakToRemove.user }).session(session);
-      
+
+      // Store the status of the break before removing it
+      let hasStarted = breakToRemove.hasStarted;
+      let hasEnded = breakToRemove.hasEnded;
+      let user = breakToRemove.user;
+
       await BreakTrack.findByIdAndRemove(id, { session });
-      
+
       const availableSlotsData = await BreakSlots.findOne().session(session);
       const availableSlots = availableSlotsData.slots;
       const activeBreaks = await BreakTrack.countDocuments({ status: 'active' }).session(session);
@@ -36,7 +41,7 @@ const removeBreak = (io, BreakTrack, User) => {
           await nextInQueue.save({ session });
         }
       }
-      
+
       let roundedRemainingBreakTime = 0;
 
       if (breakToRemove.hasStarted && !breakToRemove.hasEnded) {
@@ -45,7 +50,7 @@ const removeBreak = (io, BreakTrack, User) => {
         const timeEnjoyed = currentTime - breakStartTime;
         const remainingBreakTime = (breakToRemove.duration * 60 * 1000) - timeEnjoyed;
         const remainingBreakTimeInSeconds = remainingBreakTime / 1000;
-        
+
         if (remainingBreakTimeInSeconds >= 0 && remainingBreakTimeInSeconds < 30) {
           roundedRemainingBreakTime = 0;
         } else if (remainingBreakTimeInSeconds >= 30 && remainingBreakTimeInSeconds < 90) {
@@ -57,27 +62,27 @@ const removeBreak = (io, BreakTrack, User) => {
         userToUpdate.remainingBreakTime += roundedRemainingBreakTime;
         await userToUpdate.save({ session });
       }
-      
+
       await session.commitTransaction();
       hasCommitted = true;
 
       let actionUser = req.user;
 
-      if (breakToRemove.hasEnded) {
+      if (hasEnded) {
         io.emit('reload');
-        logger.info(`${kleur.magenta(actionUser.username)} ended ${kleur.magenta(breakToRemove.user + '\'s')} break after break end`, { username: req.user.username });
-      } else if (!breakToRemove.hasStarted && beforeStart) {
+        logger.info(`${kleur.magenta(actionUser.username)} ended ${kleur.magenta(user + '\'s')} break after break end`, { username: req.user.username });
+      } else if (!hasStarted && beforeStart) {
         io.emit('reload');
-        logger.info(`${kleur.magenta(actionUser.username)} removed ${kleur.magenta(breakToRemove.user + '\'s')} break before break start`, { username: req.user.username });
+        logger.info(`${kleur.magenta(actionUser.username)} removed ${kleur.magenta(user + '\'s')} break before break start`, { username: req.user.username });
       } else {
         io.emit('reload');
-        logger.info(`${kleur.magenta(actionUser.username)} ended ${kleur.magenta(breakToRemove.user + '\'s')} break with ${kleur.yellow(Math.floor(roundedRemainingBreakTime / 60) + ' minutes')} remaining. Remaining break time has been credited back to ${kleur.magenta(breakToRemove.user + '\'s')}'s total break time available`, { username: req.user.username });
+        logger.info(`${kleur.magenta(actionUser.username)} ended ${kleur.magenta(user + '\'s')} break with ${kleur.yellow(Math.floor(roundedRemainingBreakTime / 60) + ' minutes')} remaining. Remaining break time has been credited back to ${kleur.magenta(user + '\'s')}'s total break time available`, { username: req.user.username });
       }
-      
+
       res.sendStatus(200);
     } catch (err) {
       logger.error("Error removing break: ", err, { username: req.user.username });
-      
+      console.log(err);
       if (!hasCommitted) {
         await session.abortTransaction();
       }
