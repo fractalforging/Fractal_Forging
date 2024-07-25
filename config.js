26d528b2929104b634316161a4f4d5eee779c385
kodemystic <btarrana@protonmail.ch>
2024-07-25 16:23:34 +0200

    1

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
 export default {
     breakTime: {
-      totalMinutes: 30
+      totalMinutes: 30,
+      resetTime: {
+        hour: 22,
+        minute: 0o0
+      },
+      environment: 'production', // 'production' or 'development'
+      developmentOverride: {
+        enabled: false,
+        resetIntervalMinutes: 5
+      }
     }
-    // Add other configuration settings here as needed
   };
